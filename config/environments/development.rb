7c835740300b2390902d6006835ab1553ceab18f
Augusto Knijnik <augusto455@gmail.com>
2014-09-14 22:32:34 +0000

    teste

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options={ :host => 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
-  config.aciton_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
   
   
 end
